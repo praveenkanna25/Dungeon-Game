53d5995351064fea64b9d0bd94f08671a314a736
praveenkanna25 <131360166+praveenkanna25@users.noreply.github.com>
2023-12-24 11:06:19 +0530

    Update DungeonDemo.java
    Question 4

--- DungeonDemo.java
+++ DungeonDemo.java
@@ -1,68 +1,85 @@
 package game.org;
-
-import java.util.Scanner;
+package game.org;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class DungeonDemo {
-	 public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        System.out.println("Enter the row and column:");
-	        int row = sc.nextInt();
-	        int col = sc.nextInt();
-	        System.out.println("Enter the Adventure Position");
-	        int adr = sc.nextInt();
-	        int adc = sc.nextInt();
-	        System.out.println("Enter the Monster position:");
-	        int mtr = sc.nextInt();
-	        int mtc = sc.nextInt();
-	        System.out.println("Enter the gold Position");
-	        int gdr = sc.nextInt();
-	        int gdc = sc.nextInt();
-	        char[][] game = new char[row + 1][col + 1];
-	        game[adr][adc] = 'A';
-	        game[mtr][mtc] = 'M';
-	        game[gdr][gdc] = 'G';
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the row and column:");
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		System.out.println("Enter the Adventure Position");
+		int adr = sc.nextInt();
+		int adc = sc.nextInt();
+		System.out.println("Enter the Monster position:");
+		int mtr = sc.nextInt();
+		int mtc = sc.nextInt();
+		System.out.println("Enter the trigger position:");
+		int tgr = sc.nextInt();
+		int tgc = sc.nextInt();
 
-	        int adventurerow = -1;
-	        int adventurecol = -1;
-	        int monsterrow = -1;
-	        int monstercol = -1;
-	        int goldrow = -1;
-	        int goldcol = -1;
+		System.out.println("Enter the gold Position");
+		int gdr = sc.nextInt();
+		int gdc = sc.nextInt();
+		char[][] game = new char[row + 1][col + 1];
+		game[adr][adc] = 'A';
+		game[mtr][mtc] = 'M';
+		game[tgr][tgc] = 'A';
+		game[gdr][gdc] = 'G';
 
-	        List<String> adventurerPath = new ArrayList<>();
+		int adventurerow = -1;
+		int adventurecol = -1;
+		int monsterrow = -1;
+		int monstercol = -1;
+		int triggerrow = -1;
+		int triggercol = -1;
 
-	        for (int i = 0; i < game.length; i++) {
-	            for (int j = 0; j < game[0].length; j++) {
-	                if (game[i][j] == 'A') {
-	                    adventurerPath.add("(" + i + "," + j + ")");
-	                    adventurerow = i;
-	                    adventurecol = j;
-	                } else if (game[i][j] == 'G') {
-	                    goldrow = i;
-	                    goldcol = j;
-	                } else if (game[i][j] == 'M') {
-	                    monsterrow = i;
-	                    monstercol = j;
-	                }
-	            }
-	        }
+		int goldrow = -1;
+		int goldcol = -1;
 
-	        System.out.println("Adventurer's Path:");
-	        for (String position : adventurerPath) {
-	            System.out.println(position);
-	        }
+		for (int i = 0; i < game.length; i++) {
+			for (int j = 0; j < game[0].length; j++) {
+				if (game[i][j] == 'A') {
+					adventurerow = i;
+					adventurecol = j;
+				} else if (game[i][j] == 'G') {
+					goldrow = i;
+					goldcol = j;
+				} else if (game[i][j] == 'M') {
+					monsterrow = i;
+					monstercol = j;
 
-	        int stepadventure = Math.abs(adventurerow - goldrow) + Math.abs(adventurecol - goldcol);
-	        int stepmonster = Math.abs(monsterrow - goldrow) + Math.abs(monstercol - goldcol);
+				} else if (game[i][j] == 'T') {
+					triggerrow = i;
+					triggercol = j;
 
-	        if (stepmonster < stepadventure) {
-	            System.out.println("No Possible solution");
-	        } else
-	            System.out.println("Step to reach: " + stepadventure);
-	    }
-	
+				}
+
+			}
+		}
+		int stepadventure = Math.abs(adr - gdr) + Math.abs(adc - gdc);
+		int steptrigger = Math.abs(adr - tgr) + Math.abs(adc - tgc);
+		int stepmonster = Math.abs(mtr - gdr) + Math.abs(mtc - gdc);
+
+		System.out.println(stepadventure);
+		System.out.println(steptrigger);
+		System.out.println(stepmonster);
+
+		if (stepmonster < stepadventure) {
+			if (stepadventure < steptrigger) {
+
+				System.out.println("Number of steps: " + (stepmonster + Math.abs(mtr - gdr) + Math.abs(mtc - gdc)));
+			} else {
+
+				System.out.println("Number of steps: " + (steptrigger + Math.abs(mtr - gdr) + Math.abs(mtc - gdc)));
+
+			}
+		} else {
+			System.out.println("Adventure: Takes gold with minimum steps: " + stepadventure);
+
+		}
+	}
 }
