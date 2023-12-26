16058b923f67acd974b93e4c33334d8d4e019116
praveenkanna25 <131360166+praveenkanna25@users.noreply.github.com>
2023-12-26 11:16:03 +0530

    Update DungeonDemo.java

--- DungeonDemo.java
+++ DungeonDemo.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class DungeonDemo {
+
+public class DungeonDemo5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the row and column:");
@@ -14,72 +15,53 @@ public class DungeonDemo {
 		System.out.println("Enter the Adventure Position");
 		int adr = sc.nextInt();
 		int adc = sc.nextInt();
-		System.out.println("Enter the Monster position:");
-		int mtr = sc.nextInt();
-		int mtc = sc.nextInt();
-		System.out.println("Enter the trigger position:");
-		int tgr = sc.nextInt();
-		int tgc = sc.nextInt();
-
 		System.out.println("Enter the gold Position");
 		int gdr = sc.nextInt();
 		int gdc = sc.nextInt();
+		System.out.println("Enter The Number of Pits");
+		int countPits=sc.nextInt();
+		int a=1;
+		int[][] pits=new int[countPits][2];
+		while(countPits>0) {
+			System.out.println("Enter the pits "+ a+" row and column");
+			  pits[a - 1][0] = sc.nextInt();
+	            pits[a - 1][1] = sc.nextInt();
+			a++;
+			countPits--;
+		}
+		
 		char[][] game = new char[row + 1][col + 1];
-		game[adr][adc] = 'A';
-		game[mtr][mtc] = 'M';
-		game[tgr][tgc] = 'A';
-		game[gdr][gdc] = 'G';
+        game[adr][adc] = 'A';
+        game[gdr][gdc] = 'G';
 
-		int adventurerow = -1;
-		int adventurecol = -1;
-		int monsterrow = -1;
-		int monstercol = -1;
-		int triggerrow = -1;
-		int triggercol = -1;
+        for (int k = 0; k < pits.length; k++) {
+            game[pits[k][0]][pits[k][1]] = 'P';
+        }
 
-		int goldrow = -1;
-		int goldcol = -1;
+		int adventurerow = adr;
+		int adventurecol = adc;
+		int pitrow=-1;
+		int pitcol=-1;
+		int goldrow = gdr;
+		int goldcol = gdc;
 
 		for (int i = 0; i < game.length; i++) {
 			for (int j = 0; j < game[0].length; j++) {
-				if (game[i][j] == 'A') {
-					adventurerow = i;
-					adventurecol = j;
-				} else if (game[i][j] == 'G') {
-					goldrow = i;
-					goldcol = j;
-				} else if (game[i][j] == 'M') {
-					monsterrow = i;
-					monstercol = j;
-
-				} else if (game[i][j] == 'T') {
-					triggerrow = i;
-					triggercol = j;
-
+				
+				  if (game[i][j] == 'P') {
+					pitrow = i;
+					pitcol = j;
 				}
 
+				}
 			}
-		}
-		int stepadventure = Math.abs(adr - gdr) + Math.abs(adc - gdc);
-		int steptrigger = Math.abs(adr - tgr) + Math.abs(adc - tgc);
-		int stepmonster = Math.abs(mtr - gdr) + Math.abs(mtc - gdc);
+		
+		  int stepadventure = Math.abs(adr - gdr) + Math.abs(adc - gdc);
 
-		System.out.println(stepadventure);
-		System.out.println(steptrigger);
-		System.out.println(stepmonster);
-
-		if (stepmonster < stepadventure) {
-			if (stepadventure < steptrigger) {
-
-				System.out.println("Number of steps: " + (stepmonster + Math.abs(mtr - gdr) + Math.abs(mtc - gdc)));
-			} else {
-
-				System.out.println("Number of steps: " + (steptrigger + Math.abs(mtr - gdr) + Math.abs(mtc - gdc)));
-
-			}
-		} else {
-			System.out.println("Adventure: Takes gold with minimum steps: " + stepadventure);
-
-		}
+	        if (Math.abs(adventurerow - pitrow) <= 1 && Math.abs(adventurecol - pitcol) <= 1) {
+	            System.out.println("No Possible  Solution.");
+	        } else {
+	            System.out.println("Minimum number of steps: " + stepadventure);
+	        }
+	    }
 	}
-}
